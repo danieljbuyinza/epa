e21f7daed36fadb80a3d22694ef72e5a8587a1c2
danieljbuyinza <danieljbuyinza@gmail.com>
2019-11-25 22:39:54 +0300

    Created registration form, and added bootstrap

--- index.js
+++ index.js
@@ -7,5 +7,14 @@ const session = require("express-session");
 
 const server = express();
 server.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"))
-app.use(bodyParser.urlencoded({ extended: true }))
+server.set("views", path.join(__dirname, "views"))
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(express.static("static-files"));
+
+server.get("/register", (req, res) => {
+    res.render("register");
+})
+
+server.listen(4000, () => {
+    console.log("Server listening on port 4000");
+})
