171db4b705a0c3cef8682816a2a2230c0ddde9e1
PhyllisNabangi <nabangiphyllis7@gmail.com>
2019-11-26 18:08:56 +0300

    Linked the pages to the navbar

--- index.js
+++ index.js
@@ -26,3 +26,9 @@ app.get('/home', (req, res) => {
 app.get("/login", (req, res) => {
     res.render("login");
 })
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+app.get("/guest", (req, res) => {
+    res.render("guest");
+})
