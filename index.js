00ac5bf4514c6e81e6805bda32a15a51e8f597ab
yash45 <lebronash@gmail.com>
2019-11-26 07:43:21 +0300

    this is the home page

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
-const bcryptjs = require("bcryptjs");
-const session = require("express-session");
+//const bcryptjs = require("bcryptjs");
+//const session = require("express-session");
 
-const server = express();
-server.set("view engine", "pug");
+const app = express();
+app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/node-demo", { useNewUrlParser: true, useUnifiedTopology: true });
+
+//listening on terminal
+app.listen(4000, () => {
+    console.log("listening on 4000");
+})
+
+app.get('/home', (req, res) => {
+    res.render('home')
+})
